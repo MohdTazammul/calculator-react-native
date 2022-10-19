5d7c7fcd77fdff74613d0fe539b1cbaa834a05b9
MohdTazammul <tazammul1683@gmail.com>
2022-10-19 17:41:18 +0530

    Initiating my first react native app, that is a calculator

---
+++ App.js
@@ -0,0 +1,26 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { StyleSheet, Switch, Text, View } from 'react-native';
+import { ThemeContext } from './src/context/ThemeContext';
+
+export default function App() {
+
+  const [theme, setTheme] = useState('light');
+
+  return (
+    <View style={theme == 'light' ?styles.container : [styles.container, {backgroundColor:'black'}]}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+      <Switch value={theme == 'light'} onValueChange={()=>setTheme(theme == 'light' ? 'dark' : 'light')} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
