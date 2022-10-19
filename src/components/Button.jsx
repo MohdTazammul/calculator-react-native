e4cf8fdcdd64a38c81213cd1573d0976fa314f72
MohdTazammul <tazammul1683@gmail.com>
2022-10-19 18:20:35 +0530

    Added Button component for calculator

---
+++ src/components/Button.jsx
@@ -0,0 +1,36 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+import React, {useContext} from 'react'
+import { ThemeContext } from '../context/ThemeContext';
+import { Styles } from '../styles/style';
+
+
+const Button = ({ title, onPress, isBlue, isGray }) => {
+    const theme = useContext(ThemeContext);
+    return (
+        <TouchableOpacity 
+            style={
+                isBlue 
+                ? Styles.btnBlue 
+                : isGray 
+                ? Styles.btnGray 
+                : theme === "light" 
+                ? Styles.btnLight 
+                : Styles.btnDark
+            } 
+            onPress={onPress}>
+            <Text 
+               style={
+                   isBlue || isGray 
+                   ? Styles.smallTextLight
+                   : theme === "dark" 
+                   ? Styles.smallTextLight 
+                   : Styles.smallTextDark 
+                }
+            >
+                {title}
+            </Text>
+        </TouchableOpacity>
+    );
+}
+
+export default Button
